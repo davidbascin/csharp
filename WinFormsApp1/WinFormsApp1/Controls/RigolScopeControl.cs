3e2aa543c311d6eaa6c21a225ce6f0ae44356ec0
davidbascin <dbascin@gmail.com>
2026-02-24 05:08:01 -0500

    Added minimum code to get a message from the scope.

--- WinFormsApp1/WinFormsApp1/Controls/RigolScopeControl.cs
+++ WinFormsApp1/WinFormsApp1/Controls/RigolScopeControl.cs
@@ -5,32 +5,27 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using WinFormsApp1.Hardware;
 
 namespace WinFormsApp1
 {
     public partial class RigolScopeControl : UserControl, IStatusProvider
     {
-        private bool _isBusy = false;
         public event EventHandler<StatusChangedEventArgs> StatusChanged = (sender, e) => { };
+        private RigolScopeHardware ScopeHw = new RigolScopeHardware();
         public RigolScopeControl()
         {
             InitializeComponent();
+            ScopeHw.StatusChanged += ScopeHwStatusChanged;
+            Disposed += (_, _) => ScopeHw.StatusChanged -= ScopeHwStatusChanged;
         }
-
+        private void ScopeHwStatusChanged(object? sender, StatusChangedEventArgs e)
+        {
+            StatusChanged?.Invoke(this, e);
+        }
         private void label1_Click(object sender, EventArgs e)
         {
-            _isBusy = _isBusy ? false : true;
-
-            // TODO: Communicate with the scope, refreshing some data
-            if (_isBusy)
-            {
-                StatusChanged?.Invoke(this, new StatusChangedEventArgs("Start talking to Rigol Scope.", true));
-            }
-            else
-            {
-                StatusChanged?.Invoke(this, new StatusChangedEventArgs("Finished talking to Rigol Scope.", false));
-
-            }
+            string response = ScopeHw.Query("*IDN?", 5000, 100);
         }
     }
 }
---
+++ WinFormsApp1/WinFormsApp1/Hardware/RigolScopeHardware.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace WinFormsApp1.Hardware;
+
+internal class RigolScopeHardware: IStatusProvider
+{
+    private const string DefaultIpAddress = "192.168.50.164";
+    private string ipAddress = DefaultIpAddress;
+    public event EventHandler<StatusChangedEventArgs> StatusChanged = (sender, e) => { };
+    public RigolScopeHardware()
+    { 
+        ipAddress = SettingsManager.Settings.RigolScopeIp ?? DefaultIpAddress;
+    }
+    private void ReportStatus(string message, bool isError)
+    {
+        string status = $"Rigol Scope ({ipAddress}) {message}";
+        status = status.ReplaceLineEndings(string.Empty);
+        StatusChanged?.Invoke(this, new StatusChangedEventArgs(message, isError));
+    }
+    public string Query(string message, int timeoutMs, int maxCharacterCount)
+    {
+        if (message is null) throw new ArgumentNullException("Message must be not null.");
+        if (message.Length == 0) throw new ArgumentException("Message must be not empty.");
+        if (timeoutMs < 1 || timeoutMs > 60000) throw new ArgumentOutOfRangeException(nameof(timeoutMs), "Timeout must be between 1 and 60000 milliseconds.");
+        ReportStatus($"Query \"{message}\".", true);
+        try
+        {
+            using (TcpClient client = new TcpClient())
+            {
+                client.ReceiveTimeout = timeoutMs;
+                client.SendTimeout = timeoutMs;
+                client.Connect(ipAddress, 5025);
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] dataToSend = Encoding.ASCII.GetBytes(message + "\n");
+                    stream.Write(dataToSend, 0, dataToSend.Length);
+                    byte[] buffer = new byte[maxCharacterCount];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim();
+                    response = response.TrimEnd('\r', '\n');
+                    ReportStatus($"Response: \"{response}\".", false);
+                    return response;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            ReportStatus($"Error: {ex.Message}", true);
+            return string.Empty;
+        }
+    }
+
+}
